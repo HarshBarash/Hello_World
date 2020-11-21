762e65816aa1460bd6f305a3c4aeef5d044e93c5
HarshBarash <barashEU@yandex.ru>
2020-11-21 17:07:39 +0500

    Init Commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, { } from 'react';
+import { View, Text, Image, StyleSheet,  } from 'react-native'
+import Emoji from 'react-native-emoji';
+
+const TextExample = () => {
+
+  return (
+    <View><Text style = {styles.text} >
+        <Text style = {styles.italicText}>Hello World!</Text></Text>
+
+      <View style = {styles.container}>
+        <Image style={  styles.profileImgContainer  }
+               source={{uri: "https://www.jingu.ru/sirius/jingu-logo-512.png"}} />
+
+        <Text style = {styles.textStyle } >
+          <Text style = {styles.italicText }>
+                Антон Баранов
+          </Text >
+        </Text >
+
+        <Emoji name="coffee"  style={{fontSize: 80}}/>
+
+      </View>
+    </View>
+  )
+}
+export default TextExample
+
+const styles = StyleSheet.create ({
+  container: {
+    alignItems: 'center',
+    marginTop: 30,
+    padding: 90
+  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // text: {
+  //   color: '#41cdf4'
+           textStyle: { color: "#41cdf4", fontSize: 20
+  },
+    profileImgContainer: {
+        marginLeft: 16,
+        margin: 30,
+        height: 180,
+        width: 180,
+        borderRadius: 120,
+
+    },
+    profileImg: {
+        height: 80,
+        width: 80,
+        borderRadius: 40,
+    },
+
+  italicText: {
+    color: '#37859b',
+    fontStyle: 'italic'
+  }
+})
